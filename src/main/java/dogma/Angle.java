580a269d47c67ffd3a5567a94d0955070f100c11
Arto Bendiken <arto@bendiken.net>
2020-08-06 15:54:02 +0300

    Implemented Angle.

--- src/main/java/dogma/Angle.java
+++ src/main/java/dogma/Angle.java
@@ -2,4 +2,52 @@
 
 package dogma;
 
-public interface Angle {}
+import androidx.annotation.NonNull;
+
+public interface Angle {
+  /** Constructs an angle from degrees. */
+  @NonNull
+  public static Angle fromDegrees(final double degrees) {
+    return new InRadians(degrees / 180.0 * Math.PI);
+  }
+
+  /** Constructs an angle from radians. */
+  @NonNull
+  public static Angle fromRadians(final double radians) {
+    return new InRadians(radians);
+  }
+
+  /** Constructs an angle from turns. */
+  @NonNull
+  public static Angle fromTurns(final double turns) {
+    return new InRadians(turns * 2 * Math.PI);
+  }
+
+  /** Returns the angle in degrees. */
+  default public double getDegrees() {
+    return this.getRadians() / Math.PI * 180.0;
+  }
+
+  /** Returns the angle in radians. */
+  public double getRadians();
+
+  /** Returns the angle in turns. */
+  default public double getTurns() {
+    return this.getRadians() / (2 * Math.PI);
+  }
+
+  class InRadians implements Angle {
+    private static final long serialVersionUID = 1L;
+
+    private final double radians;
+
+    protected InRadians(final double radians) {
+      this.radians = radians;
+    }
+
+    @Override
+    public double getRadians() {
+      return this.radians;
+    }
+  }
+}
